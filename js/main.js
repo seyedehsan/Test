39facf79ad5767d85dfa261cd99d634c4bc065ec
seyedehsan <ehsan_agha@yahoo.com>
2019-03-27 22:42:10 -0400

    the first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+class User {
+    constructor(name, family, age){
+        this.name = name;
+        this.family = family;
+        this.age = age;
+    }
+    displayUser(){
+        console.log(this);
+    }
+}
+
+class Admin extends User {
+    constructor(email){
+        super
+    }
+    deleteAccount(accountName){
+        users = users.filter( user =>{
+            return user.name != accountName;
+        });
+    }
+}
+
+let userOne = new User('Ehsan', 'Seyedian', 37);
+let userTwo = new User('Majid', 'Seyedian', 50);
+let admin = new Admin('Mohsen', 'Seyedian', 45);
+
+let users = [userOne, userTwo, admin];
+
+console.log(users);
+admin.deleteAccount('Majid');
+console.log(users);
